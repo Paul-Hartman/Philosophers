57acc33c15819cf49eaeddf6e3e2c7d5d6b770de
Paul Hartman <phartman@student.42berlin.de>
2024-07-31 19:14:43 +0200

    messed around

--- philo/philo.c
+++ philo/philo.c
@@ -6,11 +6,43 @@
 /*   By: phartman <phartman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 16:44:45 by phartman          #+#    #+#             */
-/*   Updated: 2024/07/23 16:52:28 by phartman         ###   ########.fr       */
+/*   Updated: 2024/07/31 18:57:05 by phartman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+void	*philo_action(void *arg)
+{
+	t_philo	*philosopher;
+	struct timeval tv;
+
+	philosopher = (t_philo *)arg.philos;
+	while(1)
+	{
+		gettimeofday(&tv, NULL);
+		printf("◦ %ld.%06ld %d is thinking", tv.tv_sec, tv.tv_usec, philosopher->id);
+		pthread_mutex_lock(philosopher->left_fork);
+		gettimeofday(&tv, NULL);
+		printf("◦ %ld.%06ld %d has taken a fork\n", tv.tv_sec, tv.tv_usec, philosopher->id);
+		pthread_mutex_lock(philosopher->right_fork);
+		gettimeofday(&tv, NULL);
+		printf("◦ %ld.%06ld %d has taken a fork\n", tv.tv_sec, tv.tv_usec, philosopher->id);
+		gettimeofday(&tv, NULL);
+		printf("◦ %ld.%06ld %d is eating", tv.tv_sec, tv.tv_usec, philosopher->id);
+		usleep(philosopher->vars->time_to_eat);
+		gettimeofday(&tv, NULL);
+		printf("◦ %ld.%06ld %d is sleeping", tv.tv_sec, tv.tv_usec, philosopher->id);
+		usleep(philosopher->vars->time_to_sleep);
+		if(philosopher->last_meal < philosopher->vars->time_to_die)
+		{
+			gettimeofday(&tv, NULL);
+			printf("◦ %ld.%06ld %d died", gettimeofday(&tv, NULL), philosopher->id);
+			return ;
+		}
+		
+	}
+}
 
 int	ft_atoi(const char *nptr)
 {
@@ -40,27 +72,66 @@ int	ft_atoi(const char *nptr)
 	return (result * sign);
 }
 
-int main(int argc, char const *argv[])
+void	create_threads(t_vars vars)
 {
-	int nr_of_philos;
-	int time_to_die;
-	int time_to_eat;
-	int time_to_sleep;
-	int nr_of_meals;
-	
-	if(argc < 5 || argc > 6)
+	int	i;
+
+	i = 0;
+	while (i < vars.nr_of_philos)
+	{
+		pthread_create(vars.philos[i].thread, NULL, philo_action,
+			&vars);
+		i++;
+	}
+}
+
+void	create_mutexes(t_vars vars)
+{
+	int	i;
+
+	i = 0;
+	while (i < vars.nr_of_forks)
+		pthread_mutex_init(&vars.forks[i++], NULL);
+}
+
+void	join_threads(t_vars vars)
+{
+	int	i;
+
+	i = 0;
+	while (i < vars.nr_of_philos)
+		pthread_join(*vars.philos[i++].thread, NULL);
+}
+
+void	destroy_mutexes(t_vars vars)
+{
+	int	i;
+
+	i = 0;
+	while (i < vars.nr_of_forks)
+		pthread_mutex_destroy(&vars.forks[i++]);
+}
+
+int	main(int argc, char const *argv[])
+{
+	t_vars	vars;
+
+	if (argc < 5 || argc > 6)
 	{
 		printf("Error: wrong number of arguments\n");
-		return 1;
+		return (1);
 	}
-	nr_of_philos = ft_atoi(argv[1]);
-	time_to_die = ft_atoi(argv[2]);
-	time_to_eat = ft_atoi(argv[3]);
-	time_to_sleep = ft_atoi(argv[4]);
-	if(argc == 6)
-		nr_of_meals = ft_atoi(argv[5]);
+	vars.nr_of_philos = ft_atoi(argv[1]);
+	vars.time_to_die = ft_atoi(argv[2]);
+	vars.time_to_eat = ft_atoi(argv[3]);
+	vars.time_to_sleep = ft_atoi(argv[4]);
+	if (argc == 6)
+		vars.nr_of_meals = ft_atoi(argv[5]);
 	else
-		nr_of_meals = -1;
-	
-	return 0;
+		vars.nr_of_meals = -1;
+	create_mutexes(vars);
+	create_threads(vars);
+	join_threads(vars);
+	destroy_mutexes(vars);
+	return (0);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: phartman <phartman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 16:43:34 by phartman          #+#    #+#             */
-/*   Updated: 2024/07/23 16:52:05 by phartman         ###   ########.fr       */
+/*   Updated: 2024/07/31 18:14:51 by phartman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,29 @@
 # include <string.h>
 # include <limits.h>
 #include <stddef.h>
+
+typedef struct t_vars
+{
+	unsigned int nr_of_philos;
+	unsigned int nr_of_forks;
+	unsigned int time_to_die;
+	unsigned int time_to_eat;
+	unsigned int time_to_sleep;
+	unsigned int nr_of_meals;
+	pthread_mutex_t *forks;
+	t_philo *philos;
+}				t_vars;
+
+typedef struct t_philo
+{
+	pthread_t *thread;
+	unsigned int id;
+	unsigned int nr_of_meals;
+	unsigned int last_meal;
+	pthread_mutex_t *left_fork;
+	pthread_mutex_t *right_fork;
+	pthread_mutex_t *print_mutex;
+	t_vars *vars;
+}				t_philo;
 
 # endif

7535d184807213cd3ad79cd3f46e2dd0627d5725
Paul Hartman <phartman@student.42berlin.de>
2024-08-12 20:23:55 +0200

    need to add monitor thread

--- philo/philo.c
+++ philo/philo.c
@@ -6,11 +6,25 @@
 /*   By: phartman <phartman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 16:44:45 by phartman          #+#    #+#             */
-/*   Updated: 2024/08/12 18:12:11 by phartman         ###   ########.fr       */
+/*   Updated: 2024/08/12 20:09:38 by phartman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+
+int check_time_to_die(struct timeval tv, struct timeval last_meal, t_philo *philo)
+{
+	long int diff;
+	diff = (tv.tv_sec - last_meal.tv_sec) * 1000000 + tv.tv_usec - last_meal.tv_usec;
+	if(diff > philo->vars->time_to_die * 1000)
+	{
+		printf("◦ %ld.%06ld %d died\n", tv.tv_sec, tv.tv_usec, philo->id);
+		philo->vars->died = 1;
+		return (1);
+	}
+	return (0);
+}
 
 void	*philo_action(void *arg)
 {
@@ -27,53 +41,64 @@ void	*philo_action(void *arg)
 	left = id;
 	right = (id + 1) % vars->nr_of_forks;
 	int lower_fork = left < right ? left : right;
-int higher_fork = left < right ? right : left;
+	int higher_fork = left < right ? right : left;
 	
 	//id = //get frm arg somehow
 	//t_philo	*philosopher;
 	struct timeval tv;
 
 	//philosopher = (t_philo *)arg.philos;
-	while(1)
+	while(!philo->vars->died)
 	{
-		printf("got to beggining of loop\n");
 		//think
 		gettimeofday(&tv, NULL);
+		if(check_time_to_die(tv, philo->last_meal, philo))
+			break;
 		printf("◦ %ld.%06ld %d is thinking\n", tv.tv_sec, tv.tv_usec, id);
 		usleep(vars->time_to_sleep * 1000);
 		//take forks
 		
 		pthread_mutex_lock(&vars->forks[lower_fork]);
 		gettimeofday(&tv, NULL);
+		if(check_time_to_die(tv, philo->last_meal, philo))
+			break;
 		printf("◦ %ld.%06ld %d has taken a fork\n", tv.tv_sec, tv.tv_usec, id);
+		
+			
 		pthread_mutex_lock(&vars->forks[higher_fork]);
 		gettimeofday(&tv, NULL);
+		if(check_time_to_die(tv, philo->last_meal, philo))
+			break;
 		printf("◦ %ld.%06ld %d has taken a fork\n", tv.tv_sec, tv.tv_usec, id);
+		
+		
 	
 		//eat
 		gettimeofday(&tv, NULL);
+		if(check_time_to_die(tv, philo->last_meal, philo))
+			break;
+		gettimeofday(&philo->last_meal, NULL);
 		printf("◦ %ld.%06ld %d is eating\n", tv.tv_sec, tv.tv_usec, id);
 		usleep(vars->time_to_eat * 1000);
-		
-		//release forks
+				//release forks
 		pthread_mutex_unlock(&vars->forks[higher_fork]);
 		pthread_mutex_unlock(&vars->forks[lower_fork]);
+		philo->meals_eaten++;
+		if(philo->meals_eaten == vars->nr_of_meals && vars->nr_of_meals != -1)
+			break;
+			
+
 
 		//sleep
 		gettimeofday(&tv, NULL);
-		printf("◦ %ld.%06ld %d is sleeping/n", tv.tv_sec, tv.tv_usec, id);
+		if(check_time_to_die(tv, philo->last_meal, philo))
+			break;
+		printf("◦ %ld.%06ld %d is sleeping\n", tv.tv_sec, tv.tv_usec, id);
 		usleep(vars->time_to_sleep * 1000);
-
-		//check death
-		// if(philosopher->last_meal < philosopher->vars->time_to_die)
-		// {
-		// 	gettimeofday(&tv, NULL);
-		// 	printf("◦ %ld.%06ld %d died", gettimeofday(&tv, NULL), id);
-		// 	return ;
-		// }
-		printf("got to end of loop\n");
 	}
+	return (NULL);
 }
+
 
 int	ft_atoi(const char *nptr)
 {
@@ -115,6 +140,8 @@ void	create_threads(t_vars *vars)
 	{
 		philo[i].id = i;
 		philo[i].vars = vars;
+		philo[i].meals_eaten = 0;
+		gettimeofday(&philo[i].last_meal, NULL);
 		pthread_create(&vars->philos[i], NULL, philo_action,
 			&philo[i]);
 		i++;
@@ -138,6 +165,7 @@ void	join_threads(t_vars *vars)
 	i = 0;
 	while (i < vars->nr_of_philos)
 		pthread_join(vars->philos[i++], NULL);
+	free(vars->philos);
 }
 
 void	destroy_mutexes(t_vars *vars)
@@ -147,6 +175,7 @@ void	destroy_mutexes(t_vars *vars)
 	i = 0;
 	while (i < vars->nr_of_forks)
 		pthread_mutex_destroy(&vars->forks[i++]);
+	free(vars->forks);
 }
 
 
@@ -165,6 +194,7 @@ int	main(int argc, char const *argv[])
 	vars.time_to_die = ft_atoi(argv[2]);
 	vars.time_to_eat = ft_atoi(argv[3]);
 	vars.time_to_sleep = ft_atoi(argv[4]);
+	vars.died = 0;
 	if (argc == 6)
 		vars.nr_of_meals = ft_atoi(argv[5]);
 	else
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: phartman <phartman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 16:43:34 by phartman          #+#    #+#             */
-/*   Updated: 2024/08/12 17:27:19 by phartman         ###   ########.fr       */
+/*   Updated: 2024/08/12 19:51:50 by phartman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,8 @@ typedef struct t_vars
 	unsigned int time_to_die;
 	unsigned int time_to_eat;
 	unsigned int time_to_sleep;
-	unsigned int nr_of_meals;
+	int nr_of_meals;
+	int died;
 	pthread_mutex_t *forks;
 	pthread_t *philos;
 }				t_vars;
@@ -49,6 +50,8 @@ typedef struct t_vars
 typedef struct t_philo
 {
 	int id;
+	int meals_eaten;
+	struct timeval last_meal;
 	t_vars *vars;
 }				t_philo;
 
